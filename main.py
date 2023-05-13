c9ae69c923f680493fef26d3d3f26edd01f427fc
Brandon Nguyen <brandonnguyen257@gmail.com>
2023-05-13 16:29:34 -0700

    added main.py file as the main script, utils folder, parser_utils file, and added get_audio_time_series_and_audio_samplerate

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-from haptic_utils import generateTimeStampsArray
+
+from utils.haptic_utils import get_audio_time_series_and_audio_samplerate
+from utils.parser_utils import get_sample_rate_and_file_name
+import time
+
+start_time = time.time()
+
+sample_rate, file_name = get_sample_rate_and_file_name()
+
+audio_time_series, audio_samplerate = get_audio_time_series_and_audio_samplerate(file_name, save_plot=True)
 
 
-timeStampArray = generateTimeStampsArray(2, 10)
-print(timeStampArray)
+end_time = time.time()
+print(f'Done in {end_time - start_time} seconds')
