fea3efe10eb27846e2d7a8bb30c178e5cbed9373
Brandon Nguyen <brandonnguyen257@gmail.com>
2023-05-13 18:36:13 -0700

    add classes folder to git

---
+++ classes/HapticSample.py
@@ -0,0 +1,42 @@
+class HapticSample:
+    MIN_HAPTIC_VALUE = -1.0
+    MAX_HAPTIC_VALUE = 1.0
+    MIN_TIMESTAMP = 0.0
+
+    def __init__(self, time_stamp: float, haptic_value: float):
+        self.time_stamp = time_stamp
+        self.haptic_value = haptic_value
+
+    @property
+    def time_stamp(self):
+        return self._time_stamp
+
+    @time_stamp.setter
+    def time_stamp(self, value):
+        if value < HapticSample.MIN_TIMESTAMP:
+            raise ValueError("Time stamp cannot be negative")
+        self._time_stamp = value
+
+    @property
+    def haptic_value(self):
+        return self._haptic_value
+
+    @haptic_value.setter
+    def haptic_value(self, value):
+        if not HapticSample.MIN_HAPTIC_VALUE <= value <= HapticSample.MAX_HAPTIC_VALUE:
+            raise ValueError("Haptic value must be between -1.0 and 1.0")
+        self._haptic_value = value
+
+    def __str__(self):
+        return f'HapticSample({self.time_stamp}, {self.haptic_value})'
+
+    def __repr__(self):
+        return f'HapticSample({self.time_stamp}, {self.haptic_value})'
+
+    def __eq__(self, other):
+        if isinstance(other, HapticSample):
+            return self.time_stamp == other.time_stamp and self.haptic_value == other.haptic_value
+        return False
+
+    def __hash__(self):
+        return hash((self.time_stamp, self.haptic_value))
